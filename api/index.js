d2f85aa43a93dc07cd4f447f230657fd8ce7cc64
ruit-web <ruitaderrick@gmail.com>
2026-06-29 13:31:51 +0300

    Explicit builds: static-build for Vite + @vercel/node for API

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true }));
+  res.end(JSON.stringify({ ok: true, url: req.url, method: req.method }));
 };
